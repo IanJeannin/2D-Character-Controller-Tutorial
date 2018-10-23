3b14a78560f2e3153cddd774ab6d900913fa1b1f
IanJeannin <36770934+IanJeannin@users.noreply.github.com>
2018-10-22 21:36:09 -0500

    Add Ground Check

--- Assets/Scripts/CharacterControllerScript.cs
+++ Assets/Scripts/CharacterControllerScript.cs
@@ -6,8 +6,14 @@ public class CharacterControllerScript : MonoBehaviour {
 
     public float maxSpeed = 10;
     private bool facingRight = true;
+    private bool isOnGround;
+    public Transform groundCheck;
+    float groundRadius = 0.2f;
+    public LayerMask whatIsGround;
 
     Animator anim;
+
+
 
 	// Use this for initialization
 	void Start ()
@@ -18,6 +24,9 @@ public class CharacterControllerScript : MonoBehaviour {
 	// Update is called once per frame
 	void FixedUpdate ()
     {
+        isOnGround = Physics2D.OverlapCircle(groundCheck.position,groundRadius,whatIsGround);
+        anim.SetBool("Ground", isOnGround);
+
         float move = Input.GetAxis("Horizontal");
 
         anim.SetFloat("Speed", Mathf.Abs(move));
